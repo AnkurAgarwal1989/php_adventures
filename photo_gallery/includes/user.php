eac4a3acd6cd2c8b5eba947696aed7eceff67594
ankuraga <ankuraga@gmail.com>
2015-04-12 15:30:30 -0700

    User CRUD code

--- photo_gallery/includes/user.php
+++ photo_gallery/includes/user.php
@@ -17,6 +17,9 @@ class User extends DatabaseObject{
     
     
     //Unique functions for the class
+    
+    //Tis function authenticates a user by checking username and password.
+    //Returns an instance of User
     public static function authenticate($username = "", $password = ""){
         global $database;
         
@@ -28,10 +31,12 @@ class User extends DatabaseObject{
         $query .= "AND password = '{$password}' ";
         $query .= "LIMIT 1";
         
+        //Fucntion inherited from parent class
         $result_array = self::find_by_sql($query);
         return !empty($result_array) ? array_shift($result_array) : false;
     }
     
+    //Returns full name
     public function full_name(){
         if(isset($this->first_name) && isset($this->last_name)){
             return $this->first_name ." ". $this->last_name;
@@ -39,5 +44,74 @@ class User extends DatabaseObject{
         else{
             return "";
         }
-    }}
+    }
+    
+    public function save(){
+        //a new record wont have an ID, so we can use that to decide if we wanna update or create
+        //Just a convinience function...
+        //instead of calling create or update we just call save and let this line of code figure it out.
+        
+        //This also prevents recreating users by mistake
+        return isset($this->id) ? $this->update() : $this->create(); 
+    }
+    
+    //Creates a new user
+    protected function create(){
+        global $database;
+        
+        $sql = "INSERT INTO users ";
+        $sql .= "( username, password, first_name, last_name )";
+        $sql .= "VALUES (";
+        $sql .= "'" . $database->escape_value($this->username)   . "',";
+        $sql .= "'" . $database->escape_value($this->password)   . "',";
+        $sql .= "'" . $database->escape_value($this->first_name) . "',";
+        $sql .= "'" . $database->escape_value($this->last_name)  . "')";  
+        
+        if ($database->query($sql)){
+            // if the insert was successful...get the id that was inserted at.
+            $this->id = $database->insert_id();
+            return true;
+        }
+        return false;       
+    }
+    
+    //Update existing user
+    protected function update(){
+        global $database;
+        
+        //UPDATE users
+        //SET column1 = value1, column2 = value2...., columnN = valueN
+        //WHERE [condition];      
+        $sql = "UPDATE users SET ";
+        $sql .= " first_name = '" . $database->escape_value($this->first_name) . "' ," ;
+        $sql .= " last_name = '" . $database->escape_value($this->last_name) . "' ," ;
+        $sql .= " username = '" . $database->escape_value($this->username) . "' ," ;
+        $sql .= " password = '" . $database->escape_value($this->password) . "' " ;
+        $sql .= "WHERE id = " . $database->escape_value($this->id);
+        $database->query($sql);
+        
+        if ($database->affected_rows() == 1){
+            return true;
+        }
+        return false;  
+    
+    }
+    
+    //Delete existing user  
+    public function delete(){
+        global $database;
+        
+        $sql = "DELETE FROM users WHERE ";
+        $sql .= "id = ";
+        $sql .= "'" . $database->escape_value($this->id) . "' ";  
+        $sql .= "LIMIT 1";
+        $database->query($sql);
+                
+        if ($database->affected_rows() == 1){
+            return true;
+        }
+        return false;       
+    }
+  }
+
 ?>
---
+++ photo_gallery/public/admin/test.php
@@ -0,0 +1,42 @@
+<?php
+/* 
+ * Index page for admin folder
+ */
+require_once ('../../includes/initialize.php');
+
+//If person is not logged in..redirect to login page
+if(!($session->is_logged_in())){
+    redirect_to('login.php');
+}
+?>
+
+<?php include_layout_template('admin_header.php') ?>
+
+<?php 
+
+?>
+
+<?php 
+//Creating new user
+/*$new_user = new User();
+$new_user->first_name = "Dead";
+$new_user->last_name = "Pool";
+$new_user->password = "guns";
+$new_user->username = "assassin";
+if ($new_user->create())
+{
+    $id = $new_user->id;
+    echo "User Created: " . $new_user->id .PHP_EOL;
+}
+*/
+//Updating
+$user = User::find_by_id(2);
+/*$user->first_name = "Dead";
+$user->last_name = "Pool";
+$user->password = "blades";
+$user->username = "killers";*/
+if ($user->delete())
+    echo "deleted";
+?>
+        
+<?php include_layout_template('admin_footer.php') ?>
